f091a9d5e9d6a6defdeaf70dfa42c440ce304838
Spencer Colton <me@scolton.tech>
2017-01-23 13:50:20 -0500

    Add board

---
+++ src/tech/scolton/lightboard/Board.java
@@ -0,0 +1,13 @@
+package tech.scolton.lightboard;
+
+/**
+ * One-sentence class summary.
+ *
+ * @author Spencer Colton
+ * @author Maggie Mitchell
+ * @version 1.0
+ * @since 1.0
+ */
+public class Board {
+
+}
--- src/tech/scolton/lightboard/Controller.java
+++ src/tech/scolton/lightboard/Controller.java
@@ -3,14 +3,15 @@ package tech.scolton.lightboard;
 import javafx.event.ActionEvent;
 import javafx.event.EventTarget;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.*;
@@ -100,6 +101,30 @@ public class Controller {
                 this.status.setText("Error: " + e.getMessage());
             }
         }
+    }
+
+    @FXML
+    public void showConsole() throws IOException {
+        GridPane boardRoot = FXMLLoader.load(getClass().getResource("Board.fxml"));
+
+        for (int i = 0; i <= 16; i++) {
+            ColumnConstraints ci = new ColumnConstraints(50);
+            boardRoot.getColumnConstraints().add(ci);
+        }
+
+        for (int i = 0; i <= 7; i++) {
+            RowConstraints ri = new RowConstraints(50);
+            boardRoot.getRowConstraints().add(ri);
+        }
+
+        Stage consolePane = new Stage();
+        consolePane.setTitle("Console");
+
+        Scene boardScene = new Scene(boardRoot);
+        boardScene.getStylesheets().add(getClass().getResource("Board.css").toExternalForm());
+
+        consolePane.setScene(boardScene);
+        consolePane.show();
     }
 
 }
